ad53119d075e435b6d2c0dda43f1a2fd254bffad
Christian Baroni <7061887+christianbaroni@users.noreply.github.com>
2026-08-16 17:51:26 -0400

    Move backup error messages out of wallet cloud hook

--- src/features/backup/backup.ts
+++ src/features/backup/backup.ts
@@ -25,7 +25,6 @@ import {
 import { WrappedAlert as Alert } from '@/helpers/alert';
 import walletBackupStepTypes from '@/helpers/walletBackupStepTypes';
 import WalletBackupTypes from '@/helpers/walletBackupTypes';
-import { getUserError } from '@/hooks/useWalletCloudBackup';
 import * as i18n from '@/languages';
 import { logger, RainbowError } from '@/logger';
 import { clearAllStorages } from '@/model/mmkv';
@@ -36,6 +35,7 @@ import { loadWallets, refreshWalletInfo, setAllWalletsWithIdsAsBackedUp } from '
 import { openInBrowser } from '@/utils/openInBrowser';
 import { cloudPlatform } from '@/utils/platform';
 
+import { getBackupErrorMessage } from './getBackupErrorMessage';
 import { backupsStore, CloudBackupState } from './stores/backupsStore';
 
 const { DeviceUUID } = NativeModules;
@@ -180,7 +180,7 @@ export async function backupAllWalletsToCloud({
     onSuccess?.(password);
   } catch (error) {
     if (error instanceof Error) {
-      const userError = getUserError(error);
+      const userError = getBackupErrorMessage(error);
       onError?.(userError);
       captureException(error);
       analytics.track(analytics.event.backupError, {
